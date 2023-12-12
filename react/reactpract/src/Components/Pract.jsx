18713c9c9c12b93d1be39d597597d6b38c8624e2
Vishnu Priya <thepriya176@gmail.com>
2023-12-12 17:51:07 +0530

    git commit -m"message"

--- react/reactpract/src/Components/Pract.jsx
+++ react/reactpract/src/Components/Pract.jsx
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
-const [quotetext,setQuotetext]=useState(false);
+
+
+import axios from "axios";
 export default function Quotes(){
+    const [quotetext,setQuotetext]=useState(false);
+        let Url="'https://healthruwords.p.rapidapi.com/v1/quotes/"
+
+        axios.get(Url).then(response =>{
+            console.log(response.json())
+            // setQuotetext(response.json())
+
+            // console.log("response",quotetext);
+        }).catch(error =>{
+            console.log(error);
+        })
+
     return(
         <>
         <div className="container">
