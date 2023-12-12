a35c85224236cbcc2ad370dec8837d7e410e9daf
Vishnu Priya <thepriya176@gmail.com>
2023-12-12 13:58:53 +0530

    git commit -m"message"

--- react/prjrctjs/src/Components/Modal.jsx
+++ react/prjrctjs/src/Components/Modal.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import Mbox from './Mbox';
-import Dashboard from './Dashboard'; // Import your Modal component
-import profile from './Assests/member-1.png';
-export default function Modal({isModalOpen, onClose,}) {
+import Modal from './Modal'; // Import your Modal component
+export default function Modal({isModalOpen, onClose,Modala}) {
   const showHideClassName = isModalOpen ? "modal display-block" : "modal display-none";
 
   return(
     <>
     <div className={showHideClassName}>
       <section className="modal-main">
-        {}
+        {Modal}
         <button type="button" onClick={onClose}>Close</button>
       </section>
     </div>
