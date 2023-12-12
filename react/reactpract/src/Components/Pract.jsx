6e8544f07a93770de95bb0de5adb5887287b5917
Vishnu Priya <thepriya176@gmail.com>
2023-12-12 17:36:42 +0530

    git commit -m"in-message"

---
+++ react/reactpract/src/Components/Pract.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+const [quotetext,setQuotetext]=useState(false);
+export default function Quotes(){
+    return(
+        <>
+        <div className="container">
+            <div className="in-box">
+                
+            </div>
+        </div>
+        </>
+    )
+}
