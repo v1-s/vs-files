40a172b3ae39e28bea94e0d05312e47d5e8da928
Vishnu Priya <thepriya176@gmail.com>
2023-12-13 05:10:18 +0530

    git commit -m"message"

--- react/reactpract/src/Components/Pract.jsx
+++ react/reactpract/src/Components/Pract.jsx
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
-
-
+import { useEffect } from "react";
 import axios from "axios";
-export default function Quotes(){
-    const [quotetext,setQuotetext]=useState(false);
-        let Url="'https://healthruwords.p.rapidapi.com/v1/quotes/"
 
-        axios.get(Url).then(response =>{
-            console.log(response.json())
-            // setQuotetext(response.json())
-
-            // console.log("response",quotetext);
-        }).catch(error =>{
-            console.log(error);
-        })
-
-    return(
-        <>
-        <div className="container">
-            <div className="in-box">
-                
-            </div>
-        </div>
-        </>
-    )
+function getRandomQuote(quotes) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
 }
+
+export default function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    axios("https://type.fit/api/quotes")
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+    })
+  }, []);
+
+  function getNewQuote() {
+    setQuote(getRandomQuote(quotes));
+  }
+
+  return (
+    <main>
+      <h1> Quote Generator</h1>
+      <section>
+        <button onClick={getNewQuote}>New Quote</button>
+        <h3>
+          <span>“</span>
+          {quote?.text}
+        </h3>
+        <i>- {quote?.author}</i>
+      </section>
+    </main>
+  );
+}
+
