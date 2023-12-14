817f1f209a7d418cd343f3b4e3727fe3ea82b189
Vishnu Priya <thepriya176@gmail.com>
2023-12-14 09:57:00 +0530

    git commit -m"message"

---
+++ react/reactpract/src/Components/ReactPract2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function(){
+    return(
+        <>
+        <div className="container">
+            <h1>Hello</h1>
+        </div>
+        </>
+    )
+}
