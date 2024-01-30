232d149333987eda6458188e1d8391b42abdf2da
Vishnu Priya <thepriya176@gmail.com>
2024-01-30 11:51:00 +0530

    git commit -m"mesage"

--- react/projsmpl/src/App.js
+++ react/projsmpl/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import'./Components/Todo.css';
 import Todo from './Components/TodoList';
+import Useref from './Components/Useref';
 function App() {
   return (
     <div className="App">
      <Todo/>
-
+<Useref/>
     </div>
   );
 }
