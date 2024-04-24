d64ce2fa8f68b834d2e94bebe1c78a7f30871b5c
v1-s <thepriya176@gmail.com>
2024-04-24 11:01:36 +0530

    git commit -m"intial message"

--- react/samwrk/src/Components/Sam1.jsx
+++ react/samwrk/src/Components/Sam1.jsx
@@ -2,14 +2,10 @@ import React, { useState } from 'react';
 import "../Components/Sam1.css";
 
 const CallDetailsTable = () => {
-  const [openDetails, setOpenDetails] = useState({}); // State to track open details
+  const [openDetails, setOpenDetails] = useState(null); // State to track open details
 
   const handleRowClick = (id) => {
-    console.log(id);
-    setOpenDetails((prevDetails) => ({
-      ...prevDetails,
-      [id]: !prevDetails[id],
-    }));
+    setOpenDetails((prevDetails) => (prevDetails === id ? null : id));
   };
 
   const callData = [
@@ -37,6 +33,7 @@ const CallDetailsTable = () => {
     },
   ];
 
+
   return (
     <table className="table table-striped">
       <thead>
@@ -49,48 +46,48 @@ const CallDetailsTable = () => {
       </thead>
       <tbody>
         {callData.map((call) => (
-          <tr
-            key={call.id}
-            className={`clickable ${openDetails[call.id] ? 'show' : ''}`}
-            onClick={() => handleRowClick(call.id)}
-          >
-            <td>{call.name}</td>
-            <td>{call.account}</td>
-            <td>{call.calls}</td>
-            <td>{call.minutes}</td>
-          </tr>
-        ))}
-        {/* Conditionally render details table after the clicked row */}
-        {Object.keys(openDetails).map((callId) => (
-          openDetails[callId] && (
-            <tr key={`details-row-${callId}`}>
-              <td>
-                <table className="table table-striped details-table">
-                  {/* Details table content */}
-                  <thead>
-                    <tr>
-                      <th scope="col">Call Id</th>
-                      <th scope="col">Direction</th>
-                      <th scope="col">Number</th>
-                      <th scope="col">Duration</th>
-                      <th scope="col">Switch Code</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {callData.find((item) => item.id === callId).details.map((detail) => (
-                      <tr key={detail.callId}>
-                        <td>{detail.callId}</td>
-                        <td>{detail.direction}</td>
-                        <td>{detail.number}</td>
-                        <td>{detail.duration}</td>
-                        <td>{detail.switchCode}</td>
+          <>
+            <tr
+  onClick={() => handleRowClick(call.id)}
+  style={{
+    cursor: 'pointer',
+    backgroundColor: openDetails === call.id ? '#f5f5f5' : 'white'
+  }}
+>
+  <td>{call.name}</td>
+  <td>{call.account}</td>
+  <td>{call.calls}</td>
+  <td>{call.minutes}</td>
+</tr>
+            {openDetails === call.id && (
+              <tr>
+                <td colSpan={4}>
+                  <table className="table table-striped details-table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Call Id</th>
+                        <th scope="col">Direction</th>
+                        <th scope="col">Number</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">Switch Code</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          )
+                    </thead>
+                    <tbody>
+                      {call.details.map((detail) => (
+                        <tr key={detail.callId}>
+                          <td>{detail.callId}</td>
+                          <td>{detail.direction}</td>
+                          <td>{detail.number}</td>
+                          <td>{detail.duration}</td>
+                          <td>{detail.switchCode}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            )}
+          </>
         ))}
       </tbody>
     </table>
@@ -98,5 +95,6 @@ const CallDetailsTable = () => {
 };
 
 export default CallDetailsTable;
+
 
 
