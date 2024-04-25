ab032403cc159d341c8154296530691ded75fde3
v1-s <thepriya176@gmail.com>
2024-04-25 11:02:02 +0530

    git commit -m"intial commit"

---
+++ react/samwrk/src/Components/Reducfun.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useReducer } from "react";
+const [users,dispatch]=useReducer(Method,userData);
+const userData=[
+    {
+        id:1,
+        name:"Vennela",
+        country:"Lkw"
+
+    },
+    {
+        id:2,
+        name:"Vennela",
+        country:"Lkw"
+
+
+    },
+    {
+        id:3,
+        name:"Vennela",
+        country:"Lkw"
+
+    },
+
+]
