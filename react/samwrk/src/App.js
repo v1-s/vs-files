77da662574b440ecceb1e83749930f353dafbb40
v1-s <thepriya176@gmail.com>
2024-04-27 08:02:05 +0530

    git commit -m "intial commit of skills animation"

--- react/samwrk/src/App.js
+++ react/samwrk/src/App.js
@@ -1,30 +1,25 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CallDetailsTable from './Components/Sam1';
 import UserDashboard from './Components/Sam2';
 import Routes from './Components/routes';
 import AuthProvider from './Components/Provider/authprovider';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '../src/Components/Login';
 import HomePage from './Components/HomePage';
-import ProtectedRoute from "../src/Components/routes/ProtectedRoute";
+// import ProtectedRoute from "../src/Components/routes/ProtectedRoute";
 function App() {
   return (
     <>
-      {/* <UserDashboard/> */}
-{/* <AuthProvider>
-      <Router>
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <ProtectedRoute path="/" component={HomePage} />
-        </Switch>
-      </Router>
-    </AuthProvider> */}
-    {/* <AuthProvider>
-      <Routes/>
-    </AuthProvider> */}
-    <CallDetailsTable/>
+
+
+
+      {/* {/* <UserDashboard/> */}
+       {/* <CallDetailsTable/> */}
+       <HomePage/>
+   
+
+   
   
     </>
   );
--- react/samwrk/src/Components/HomePage.jsx
+++ react/samwrk/src/Components/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const HomePage = () => {
-  return
-   <div>
-    Home Page (Protected)
-    </div>;
+  return(
+    <>
+    <div>
+      <h1>HomePage</h1>
+    </div>
+    </>
+  )
+  
 };
 
 export default HomePage;
